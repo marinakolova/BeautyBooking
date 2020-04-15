62c47654a2652b3fe7cdde8f18d1ea848f13c782
marinakolova <marina.kolova.359@gmail.com>
2020-04-15 23:35:58 +0300

    more work on categories

---
+++ Web/BeautyBooking.Web.ViewModels/Administration/Dashboard/CategoryInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BeautyBooking.Web.ViewModels.Administration.Dashboard
+{
+    public class CategoryInputModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
