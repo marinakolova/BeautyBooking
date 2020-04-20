50beeabafa66cd5c606700ab9976ad44656186c9
marinakolova <marina.kolova.359@gmail.com>
2020-04-20 18:26:14 +0300

    FIX

---
+++ Web/BeautyBooking.Web.ViewModels/Administration/Services/ServiceSalons.cs
@@ -0,0 +1,7 @@
+﻿namespace BeautyBooking.Web.ViewModels.Administration.Services
+{
+    public class ServiceSalons
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Web/BeautyBooking.Web.ViewModels/Salons/SalonServicesViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BeautyBooking.Web.ViewModels.Salons
+{
+    using BeautyBooking.Data.Models;
+    using BeautyBooking.Services.Mapping;
+
+    public class SalonServicesViewModel : IMapFrom<SalonService>
+    {
+        public int SalonId { get; set; }
+
+        public string SalonName { get; set; }
+
+        public int ServiceId { get; set; }
+
+        public string ServiceName { get; set; }
+    }
+}
