d5aad9a546b7abf65f4d3585eda1e4b60c6a6b62
marinakolova <marina.kolova.359@gmail.com>
2020-04-28 03:04:35 +0300

    Scaffold Identity Pages. TODO: Customize them

---
+++ Web/BeautyBooking.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(BeautyBooking.Web.Areas.Identity.IdentityHostingStartup))]
+namespace BeautyBooking.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
